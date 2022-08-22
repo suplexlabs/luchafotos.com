b26fd3dd1acc6f653d5406d1bbd3bc6206cf4e21
Rich Perez <imperez5@gmail.com>
2022-08-21 22:37:58 -0400

    fix typo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,7 +14,7 @@
         <footer class="flex flex-wrap-reverse gap-10 py-10 px-4 font-bold mt-20 bg-stone-900 text-white">
             <div>
                 <ul class="flex flex-wrap gap-4 list-none p-0">
-                    <li class="text-gray-400">&copy; 2022. Lucha Fotos. A <a class="underline" target="_blank" href="https://www.wrestlefive.com">Wrestle Five</a> game.</li>
+                    <li class="text-gray-400">&copy; 2022. Lucha Fotos. A <a class="underline" target="_blank" href="https://www.wrestlefive.com">Wrestle Five</a> site.</li>
                 </ul>
             </div>
         </footer>
