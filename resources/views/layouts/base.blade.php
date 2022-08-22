4fea00357cba80491220842b4d5398a92161a47f
Rich Perez <imperez5@gmail.com>
2022-08-22 02:22:05 +0000

    added footer

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,7 +12,16 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-brand-dark text-white">
-        @yield('content')
+        <main>
+            @yield('content')
+        </main>
+        <footer class="flex flex-wrap-reverse gap-10 py-10 px-4 font-bold mt-20 bg-stone-900 text-white">
+            <div>
+                <ul class="flex flex-wrap gap-4 list-none p-0">
+                    <li class="text-gray-400">&copy; 2022. Lucha Fotos. A <a class="underline" target="_blank" href="https://www.wrestlefive.com">Wrestle Five</a> game.</li>
+                </ul>
+            </div>
+        </footer>
 
         @if(config('app.env') == 'production')
             <script defer data-domain="luchafotos.com" src="https://plausible.io/js/plausible.js"></script>
