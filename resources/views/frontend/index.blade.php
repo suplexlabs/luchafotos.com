63855299eba2b46485dac3ccbafcc0f15aa357ec
Rich Perez <imperez5@gmail.com>
2022-08-28 22:33:43 -0400

    Updated messaging

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,7 +4,4 @@
     <div class="mx-auto max-w-2xl text-center">
         <img src="/img/luchafotos-logo.png" alt="The Lucha Fotos logo" />
         <p class="mt-4 text-2xl mx-20">
-            An upcoming site that helps you find the wrestling photos you need for articles, podcast artwork, social media & more.
-        </p>
-    </div>
-@endsection
+            An upcoming search engine that helps you find the wrestling photos you love without the hassle. 
