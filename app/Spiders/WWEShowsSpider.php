c7100da9db348117dcd85027446d4fa74de84134
Rich Perez <imperez5@gmail.com>
2022-09-14 01:42:06 +0000

    started on wwe shows crawler

--- app/Spiders/WWEShowsSpider.php
+++ app/Spiders/WWEShowsSpider.php
@@ -4,7 +4,9 @@ namespace App\Spiders;
 
 use App\Spiders\Traits\HasSource;
 use Generator;
+use App\Datas\ImageData;
 use RoachPHP\Http\Response;
+use Symfony\Component\DomCrawler\Image;
 
 class WWEShowsSpider extends JavascriptSpider
 {
@@ -16,7 +18,62 @@ class WWEShowsSpider extends JavascriptSpider
 
     public function parse(Response $response): Generator
     {
-        // include archives
-        // example: https://www.wwe.com/shows/wwenxt/archive
+        $anchors = $response->filter('.wwe-shows-hub--show')->links();
+        foreach ($anchors as $anchor) {
+            yield $this->request('GET', $anchor->getUri(), 'parseShowPage');
+        }
+    }
+
+    public function parseShowPage(Response $response): Generator
+    {
+        $anchors = $response->filter('.show-contextual__speech-bubble.results .show-contextual__logo a')->links();
+        foreach ($anchors as $anchor) {
+            yield $this->request('GET', $anchor->getUri(), 'parseResultPage');
+        }
+    }
+
+    public function parseResultPage(Response $response): Generator
+    {
+        $source = $this->getSource();
+
+        $images = $response->filter('.episode-feed-card--primary-img img')->images();
+        foreach ($images as $image) {
+            $data = $this->getImageData($response, $image);
+            dd($data);
+
+            $found = $source->images()->where('url', $data->url)->first();
+            if ($found) {
+                yield $this->item([]);
+                return;
+            }
+
+            $this->dispatchJob($data, $source);
+
+            yield $this->item($data->toArray());
+        }
+    }
+
+    protected function getImageData(Response $response, Image $image): ImageData|null
+    {
+        $pageUrl = $response->getUri();
+        $components = parse_url($pageUrl);
+        $domain = $components['host'];
+
+        $title = $image->getNode()->getAttribute('alt');
+        $poster = $image->getNode()->getAttribute('data-srcset');
+        $url = explode(' ', $poster)[0];
+
+        if (substr($url, 0, 1) == '/') {
+            $url = 'https://' . $domain . $url;
+        }
+
+        $info = ImageData::from([
+            'title'   => $title,
+            'url'     => $url,
+            'pageUrl' => $pageUrl,
+            'domain'  => $domain
+        ]);
+
+        return $info;
     }
 }
