917bcf5b8cfc48860cf97fc516affab8f55b4718
Rich Perez <imperez5@gmail.com>
2022-09-26 15:16:41 +0000

    added schedule for new spider

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('roach:run WWEVideosSpider')->everyFourHours();
         $schedule->command('roach:run WWEShowsSpider')->everyThreeHours();
         $schedule->command('roach:run ImpactPhotosSpider')->weekly();
+        $schedule->command('roach:run ImpactSpider')->daily();
         // $schedule->command('roach:run AEWPhotosSpider')->daily();
 
         // backups
