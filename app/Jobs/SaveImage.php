1433d2e12562d65787046cc0007a16d505bf8f2d
Rich Perez <imperez5@gmail.com>
2022-10-03 00:53:58 +0000

    moved logic into repository class

--- app/Jobs/SaveImage.php
+++ app/Jobs/SaveImage.php
@@ -3,32 +3,21 @@
 namespace App\Jobs;
 
 use App\Datas\ImageData;
-use App\Models\Image;
-use App\Models\Page;
-use App\Models\Site;
 use App\Models\Source;
-use Carbon\Carbon;
+use App\Repositories\ImageRepository;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Intervention\Image\Exception\NotReadableException;
-use Intervention\Image\Facades\Image as FacadesImage;
 
 class SaveImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * @var \App\Datas\ImageData $data
-     */
-    protected $data;
-
-    /**
-     * @var \App\Models\Source $source
-     */
-    protected $source;
+    protected ImageData $data;
+    protected Source $source;
+    protected ImageRepository $repository;
 
     /**
      * Create a new job instance.
@@ -39,6 +28,8 @@ class SaveImage implements ShouldQueue
     {
         $this->data = $data;
         $this->source = $source;
+
+        $this->repository = app(ImageRepository::class);
     }
 
     public function handle()
@@ -50,40 +41,6 @@ class SaveImage implements ShouldQueue
             return;
         }
 
-        $url = $data->url;
-        try {
-            $img = FacadesImage::make($url);
-        } catch (NotReadableException $e) {
-            // if the image is not reaable then skip it
-            return;
-        }
-
-        $headers = get_headers($url, true);
-        $publishDate = Carbon::parse($headers['Date']);
-
-        $etag = data_get($headers, 'ETag', md5($url));
-        $etag = str($etag)->replace('"', '');
-
-        $imageExists = $source->images()->where('etag', $etag)->exists();
-        if ($imageExists) {
-            return;
-        }
-
-        $site = Site::updateOrCreate(['domain' => $data->domain]);
-        $page = Page::updateOrCreate(['site_id' => $site->id, 'url' => $data->pageUrl, 'title' => $data->title]);
-
-        Image::create([
-            'source_id'    => $source->id,
-            'site_id'      => $site->id,
-            'page_id'      => $page->id,
-            'url'          => $url,
-            'title'        => $data->title,
-            'etag'         => $etag,
-            'height'       => $img->height(),
-            'width'        => $img->width(),
-            'published_at' => $publishDate
-        ]);
-
-        // create smaller versions to use for search results
+        $this->repository->createByData($source, $data);
     }
 }
