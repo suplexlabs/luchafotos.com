30816a81d5b381bd7f4e84ca8fd2f09eb3a811f5
Rich Perez <imperez5@gmail.com>
2022-10-04 01:27:39 +0000

    added simple logic for tagging company and wrestler

--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Enums\Companies;
 use App\Models\Image;
 use App\Models\Tag;
 use Illuminate\Support\Collection;
@@ -10,17 +11,71 @@ class TagService
 {
     public function createTagsByImage(Image $image): Collection
     {
+        $tags = collect([]);
+
+        $tag = $this->createCompanyTag($image);
+        if ($tag) {
+            $tags->push($tag);
+        }
+        $tag = $this->createEventTag($image);
+        if ($tag) {
+            $tags->push($tag);
+        }
+        $tag = $this->createCompanyTag($image);
+        if ($tag) {
+            $tags->push($tag);
+        }
+        $tag = $this->createWrestlerTag($image);
+        if ($tag) {
+            $tags->push($tag);
+        }
+
+        return $tags;
     }
 
-    public function createCompanyTag(string $value): Tag
+    public function createCompanyTag(Image $image): Tag|null
     {
+        $tag = null;
+        $domain = $image->site->domain;
+
+        $companies = Companies::cases();
+        foreach ($companies as $company) {
+            if (stripos($domain, $company->value)) {
+                $tag = Tag::updateOrCreate([
+                    'name' => $company,
+                    'code' => str($company)->slug()
+                ]);
+                break;
+            }
+        }
+
+        return $tag;
     }
 
-    public function createEventTag(string $value): Tag
+    public function createEventTag(Image $image): Tag|null
     {
+        $tag = null;
+
+        return $tag;
     }
 
-    public function createWrestlerTag(string $value): Tag
+    public function createWrestlerTag(Image $image): Tag|null
     {
+        $tag = null;
+
+        // check the title for wrestler name
+        if (stripos($image->title, ' - ')) {
+            $names = str($image->title)->explode(' - ');
+            $name = count($names) == 2 ? $names[0] : 0;
+
+            if ($name) {
+                $tag = Tag::updateOrCreate([
+                    'name' => $name,
+                    'code' => str($name)->slug()
+                ]);
+            }
+        }
+
+        return $tag;
     }
 }
