5bd2221eda822c1e4d5110c65e4f74c6bdba4198
Rich Perez <imperez5@gmail.com>
2022-10-25 13:08:54 +0000

    make sure search input has no rounded borders

--- public/build/assets/Layout.10246cf6.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./app.fc4cb136.js";function n({children:t}){return e.createElement("main",null,t)}export{n as default};
---
+++ public/build/assets/Layout.dfb82f01.js
@@ -0,0 +1,1 @@
+import{R as e}from"./app.eb3b6718.js";function n({children:t}){return e.createElement("main",null,t)}export{n as default};
