8ca647bdcf5b70b0422c7886c8ace19ff9055313
Rich Perez <imperez5@gmail.com>
2022-10-29 13:13:34 +0000

    fixed feature tests

--- tests/Feature/TagsEndpointTest.php
+++ tests/Feature/TagsEndpointTest.php
@@ -12,16 +12,32 @@ test('has endpoint', function () {
     $this->assertEmpty($response->exception);
 });
 
+test('has no results', function () {
+    /** @var \Illuminate\Http\Response $response */
+    $response = get(route('tags.similar', ['tag' => null]));
+    $json = $response->json();
+
+    expect($json)->toBeArray();
+
+    $results = collect(data_get($json, 'results', []));
+
+    $resultTag = $results->first();
+
+    expect($resultTag)->toBeNull();
+    expect($results)->toBeEmpty();
+});
+
 test('has correct results', function () {
     $tag = Tag::factory()->create();
 
     // NOTE: must commit inserts to make sure test works
     \DB::commit();
 
-    /** @var \Illuminate\Http\Response $response */
-    $response = get(route('tags.similar', ['tag' => $tag->name]));
-    $json = $response->json();
+    $name = substr($tag->name, 0, 4);
 
+    /** @var \Illuminate\Http\Response $response */
+    $response = get(route('tags.similar', ['tag' => $name]));
+    $json = $response->json();
 
     expect($json)->toBeArray();
 
@@ -29,5 +45,6 @@ test('has correct results', function () {
     $resultTag = $results->first();
 
     expect($resultTag)->toBeArray();
+    expect($resultTag['name'])->toBeTruthy();
     $this->assertEquals($tag->id, $resultTag['id']);
 });
